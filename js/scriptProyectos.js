841577178111d773d1e6da2d27828fc0eaf7a1ff
Carlos Avilán <cdavilan@gmail.com>
2024-11-09 15:41:00 -0300

    Se completó la información de actores para el local storage

--- js/scriptProyectos.js
+++ js/scriptProyectos.js
@@ -102,6 +102,10 @@ const actores = [
                 pelicula: "Pulp Fiction",
                 personaje: "Pumpkin",
             },
+            {
+                pelicula: "The Hateful Eight",
+                personaje: "Oswaldo Mobray",
+            },
         ]
     },
     {
@@ -111,6 +115,18 @@ const actores = [
             {
                 pelicula: "Reservoir Dogs",
                 personaje: "Mr. Blonde",
+            },
+            {
+                pelicula: "Kill Bill Vol.1",
+                personaje: "Budd",
+            },
+            {
+                pelicula: "Kill Bill Vol.2",
+                personaje: "Budd",
+            },
+            {
+                pelicula: "The Hateful Eight",
+                personaje: "Joe Gage",
             },
         ]
     },
@@ -152,6 +168,14 @@ const actores = [
                 pelicula: "Pulp Fiction",
                 personaje: "Mia Wallace",
             },
+            {
+                pelicula: "Kill Bill Vol.1",
+                personaje: "The Bride",
+            },
+            {
+                pelicula: "Kill Bill Vol.2",
+                personaje: "The Bride",
+            },
         ]
     },
     {
@@ -161,6 +185,18 @@ const actores = [
             {
                 pelicula: "Pulp Fiction",
                 personaje: "Jules Winnfield",
+            },
+            {
+                pelicula: "Jackie Brown",
+                personaje: "Ordell Robbie",
+            },
+            {
+                pelicula: "Django Unchained",
+                personaje: "Stephen",
+            },
+            {
+                pelicula: "The Hateful Eight",
+                personaje: "Major Marquis Warren",
             },
         ]
     },
@@ -191,6 +227,312 @@ const actores = [
             {
                 pelicula: "Pulp Fiction",
                 personaje: "Marsellus Wallace",
+            },
+        ]
+    },
+    {
+        nombre: "Pam",
+        apellido: "Grier",
+        roles: [
+            {
+                pelicula: "Jackie Brown",
+                personaje: "Jackie Brown",
+            },
+        ]
+    },
+    {
+        nombre: "Robert",
+        apellido: "Forster",
+        roles: [
+            {
+                pelicula: "Jackie Brown",
+                personaje: "Max Cherry",
+            },
+        ]
+    },
+    {
+        nombre: "Bridget",
+        apellido: "Fonda",
+        roles: [
+            {
+                pelicula: "Jackie Brown",
+                personaje: "Melanie",
+            },
+        ]
+    },
+    {
+        nombre: "Michael",
+        apellido: "Keaton",
+        roles: [
+            {
+                pelicula: "Jackie Brown",
+                personaje: "Ray Nicolette",
+            },
+        ]
+    },
+    {
+        nombre: "Robert",
+        apellido: "De Niro",
+        roles: [
+            {
+                pelicula: "Jackie Brown",
+                personaje: "Louis Gara",
+            },
+        ]
+    },
+    {
+        nombre: "David",
+        apellido: "Carradine",
+        roles: [
+            {
+                pelicula: "Kill Bill Vol.1",
+                personaje: "Bill",
+            },
+            {
+                pelicula: "Kill Bill Vol.2",
+                personaje: "Bill",
+            },
+        ]
+    },
+    {
+        nombre: "Daryl",
+        apellido: "Hannah",
+        roles: [
+            {
+                pelicula: "Kill Bill Vol.1",
+                personaje: "Elle Driver",
+            },
+            {
+                pelicula: "Kill Bill Vol.2",
+                personaje: "Elle Driver",
+            },
+        ]
+    },
+    {
+        nombre: "Luci",
+        apellido: "Liu",
+        roles: [
+            {
+                pelicula: "Kill Bill Vol.1",
+                personaje: "O-Ren Ishii",
+            },
+        ]
+    },
+    {
+        nombre: "Vivica A.",
+        apellido: "Fox",
+        roles: [
+            {
+                pelicula: "Kill Bill Vol.1",
+                personaje: "Vernita Green",
+            },
+            {
+                pelicula: "Kill Bill Vol.2",
+                personaje: "Vernita Green",
+            },
+        ]
+    },
+    {
+        nombre: "Kurt",
+        apellido: "Russell",
+        roles: [
+            {
+                pelicula: "Death Proof",
+                personaje: "Stuntman Mike",
+            },
+            {
+                pelicula: "The Hateful Eight",
+                personaje: "John Ruth",
+            },
+        ]
+    },
+    {
+        nombre: "Zoë",
+        apellido: "Bell",
+        roles: [
+            {
+                pelicula: "Death Proof",
+                personaje: "Zoë Bell",
+            },
+        ]
+    },
+    {
+        nombre: "Rosario",
+        apellido: "Dawson",
+        roles: [
+            {
+                pelicula: "Death Proof",
+                personaje: "Abernathy",
+            },
+        ]
+    },
+    {
+        nombre: "Brad",
+        apellido: "Pitt",
+        roles: [
+            {
+                pelicula: "Inglourious Basterds",
+                personaje: "Lt. Aldo Raine",
+            },
+            {
+                pelicula: "Once Upon a Time in Hollywood",
+                personaje: "Cliff Booth",
+            },
+        ]
+    },
+    {
+        nombre: "Diane",
+        apellido: "Kruger",
+        roles: [
+            {
+                pelicula: "Inglourious Basterds",
+                personaje: "Bridget von Hammersmark",
+            },
+        ]
+    },
+    {
+        nombre: "Christoph",
+        apellido: "Waltz",
+        roles: [
+            {
+                pelicula: "Inglourious Basterds",
+                personaje: "Col. Hans Landa",
+            },
+            {
+                pelicula: "Django Unchained",
+                personaje: "Dr. King Schultz",
+            },
+        ]
+    },
+    {
+        nombre: "Mélanie",
+        apellido: "Laurent",
+        roles: [
+            {
+                pelicula: "Inglourious Basterds",
+                personaje: "Shosanna",
+            },
+        ]
+    },
+    {
+        nombre: "Michael",
+        apellido: "Fassbender",
+        roles: [
+            {
+                pelicula: "Inglourious Basterds",
+                personaje: "Lt. Archie Hicox",
+            },
+        ]
+    },
+    {
+        nombre: "Jamie",
+        apellido: "Foxx",
+        roles: [
+            {
+                pelicula: "Django Unchained",
+                personaje: "Django",
+            },
+        ]
+    },
+    {
+        nombre: "Leonardo",
+        apellido: "DiCaprio",
+        roles: [
+            {
+                pelicula: "Django Unchained",
+                personaje: "Calvin Candie",
+            },
+            {
+                pelicula: "Once Upon a Time in Hollywood",
+                personaje: "Rick Dalton",
+            },
+        ]
+    },
+    {
+        nombre: "Kerry",
+        apellido: "Washington",
+        roles: [
+            {
+                pelicula: "Django Unchained",
+                personaje: "Broomhilda von Shaft",
+            },
+        ]
+    },
+    {
+        nombre: "Walton",
+        apellido: "Goggins",
+        roles: [
+            {
+                pelicula: "Django Unchained",
+                personaje: "Billy Crash",
+            },
+            {
+                pelicula: "The Hateful Eight",
+                personaje: "Sheriff Chris Mannix",
+            },
+        ]
+    },
+    {
+        nombre: "Jennifer Jason",
+        apellido: "Leigh",
+        roles: [
+            {
+                pelicula: "The Hateful Eight",
+                personaje: "Daisy Domergue",
+            },
+        ]
+    },
+    {
+        nombre: "Demian",
+        apellido: "Bichir",
+        roles: [
+            {
+                pelicula: "The Hateful Eight",
+                personaje: "Bob",
+            },
+        ]
+    },
+    {
+        nombre: "Bruce",
+        apellido: "Dern",
+        roles: [
+            {
+                pelicula: "The Hateful Eight",
+                personaje: "General Sandy Smithers",
+            },
+            {
+                pelicula: "Once Upon a Time in Hollywood",
+                personaje: "George Spahn",
+            },
+        ]
+    },
+    {
+        nombre: "Margot",
+        apellido: "Robbie",
+        roles: [
+            {
+                pelicula: "Once Upon a Time in Hollywood",
+                personaje: "Sharon Tate",
+            },
+        ]
+    },
+    {
+        nombre: "Margaret",
+        apellido: "Qualley",
+        roles: [
+            {
+                pelicula: "Once Upon a Time in Hollywood",
+                personaje: "Pussycat",
+            },
+        ]
+    },
+    {
+        nombre: "Al",
+        apellido: "Pacino",
+        roles: [
+            {
+                pelicula: "Once Upon a Time in Hollywood",
+                personaje: "Marvin Schwarz",
             },
         ]
     },
