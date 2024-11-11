96f922a0883890bec4e2bf763084e4946e1e051b
Carlos Avilán <cdavilan@gmail.com>
2024-11-11 16:13:18 -0300

    Arreglo de rotación de flechas en los títulos

--- js/scriptProyectos.js
+++ js/scriptProyectos.js
@@ -2,24 +2,24 @@
 
 const titulos = document.querySelectorAll(".botonLista");
 
+
 for (let titulo of titulos) {
     titulo.addEventListener("click", () => {
-        const listas = document.querySelectorAll(".listas ul");
-
-        for (let ul of listas) {
-            if (ul !== titulo.nextElementSibling) {
-                ul.style.display = "none";
-            }
-        }
-
+        // const listas = document.querySelectorAll(".listas ul");
         let lista = titulo.nextElementSibling;
         
-        if (lista && lista.tagName === "UL") {
-            lista.style.display = lista.style.display === "block" ? "none" : "block";
-            titulo.classList.toggle("active");
-        } 
+        lista.style.display = lista.style.display === "block" ? "none" : "block";
+        titulo.classList.toggle("active");
+
+        for (let otroTitulo of titulos) {
+            if (otroTitulo !== titulo) {
+                otroTitulo.nextElementSibling.style.display = "none";
+                otroTitulo.classList.remove("active");
+            }
+        }
     });
 }
+
 
 const formulario = document.getElementById("formulario");
 const inputBusqueda = document.getElementById("inputBusqueda");
