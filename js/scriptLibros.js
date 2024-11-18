13bb923d48ba652924d0bebb43899e785fa19327
MolinaG96 <molinagomezgonzalo@gmail.com>
2024-11-18 20:54:28 -0300

    feat: peliculas y libros actualizados

---
+++ js/scriptLibros.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import { libros } from "../datos/libros.js";
+
+function renderizarLibros() {
+    const librosContainer = document.getElementById("libros-container");
+    console.log(libros);
+
+    librosContainer.innerHTML = '';
+
+    libros.forEach(libro => {
+        const tarjeta = document.createElement("div");
+        tarjeta.classList.add("libros-card");  
+
+        const imagen = document.createElement("img");
+        imagen.src = libro.imagen;
+        imagen.alt = libro.alt;
+        imagen.classList.add("poster-libros"); 
+
+        const info = document.createElement("div");
+        info.classList.add("libro-info");  
+
+        const titulo = document.createElement("h3");
+        titulo.textContent = libro.titulo;
+
+        const descripcion = document.createElement("p");
+        descripcion.textContent = libro.descripcion;
+
+        info.appendChild(titulo);
+        info.appendChild(descripcion);
+
+        tarjeta.appendChild(imagen);
+        tarjeta.appendChild(info);
+
+        librosContainer.appendChild(tarjeta);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderizarLibros();
+});
